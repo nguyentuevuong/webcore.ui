793c27391553bb5bc22d12087aa9d254dd4b287a
Nguyễn Tuệ Vương <vungtroixa@gmail.com>
2018-08-17 22:41:10 +0700

    add i18n-lang component

--- ClientApp/components/_cp-loader.ts
+++ ClientApp/components/_cp-loader.ts
@@ -1,4 +1,5 @@
 import './app-root/app-root';
+import './language/language';
 import './no-comp/no-comp';
 import './top-menu/top-menu';
 import './nav-menu/nav-menu';
---
+++ ClientApp/components/language/language.ts
@@ -0,0 +1,23 @@
+import * as ko from 'knockout';
+import * as _ from 'lodash';
+
+import { lang, i18n } from '../../common/app-i18n';
+import { component } from '../../decorator/component';
+
+@component({
+    name: 'i18n-lang',
+    template: `<li class="nav-item dropdown">
+        <a class="nav-item nav-link dropdown-toggle mr-md-2" href="#" id="bd-versions" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <i class="fa fa-globe"></i>
+            <span data-bind="i18n: '#language'"></span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" data-bind="foreach: regions">
+            <a class="dropdown-item" href="#" data-bind="i18n: '#' + $data, click: $vm.lang.bind($vm.lang, $data)"></a>
+        </div>
+    </li>`
+})
+export class LanguageComponent {
+    lang: KnockoutObservable<string> = lang;
+    regions: KnockoutObservableArray<string> = ko.observableArray(_.keys(i18n));
+}
