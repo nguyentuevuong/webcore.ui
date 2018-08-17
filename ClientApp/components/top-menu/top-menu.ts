74948e447d40ae15bfa4d23a9c9297784b3aacb8
Nguyễn Tuệ Vương <vungtroixa@gmail.com>
2018-08-17 22:42:50 +0700

    using i18n-lang component

--- ClientApp/components/top-menu/top-menu.ts
+++ ClientApp/components/top-menu/top-menu.ts
@@ -1,7 +1,6 @@
 import * as _ from 'lodash';
 import * as ko from 'knockout';
 
-import { lang, i18n } from '../../common/app-i18n';
 import { component } from "../../decorator/component";
 import { IRoute, Router, Routes } from "../../common/router";
 
@@ -13,12 +12,8 @@ import { IRoute, Router, Routes } from "../../common/router";
 export class TopMenuViewModel {
     router: Router;
     routes: IRoute[] = Routes;
-
-    lang: KnockoutObservable<string> = lang;
-    regions: KnockoutObservableArray<string> = ko.observableArray([]);
-
+    
     constructor(params: any) {
-        this.regions(_.keys(i18n));
 
         this.router = params.router;
     }
