8a54c2849b36d085dfa2a610ed1cba502e1ca244
Nguyễn Tuệ Vương <vungtroixa@gmail.com>
2018-08-21 23:26:48 +0700

    memento for boot-server

--- ClientApp/boot.server.ts
+++ ClientApp/boot.server.ts
@@ -1,1 +1,16 @@
 // code for ssr
+/*
+import * as domino from 'domino';
+import { Promise } from 'es6-promise';
+import { createServerRenderer, RenderResult, BootFuncParams, BootFunc } from 'aspnet-prerendering';
+
+export default createServerRenderer(((params: BootFuncParams) => {
+    let window = domino.createWindow(require('./index.html'), 'http://localhost:9000');
+
+    return new Promise<RenderResult>((resolve, reject) => {
+        resolve({
+            html: window.document.querySelector("body>div")!.outerHTML
+        });
+    });
+}) as BootFunc);
+*/
