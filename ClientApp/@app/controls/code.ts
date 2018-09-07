5e6f49f6465448b8051ba092f183e30cc93d7e4b
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2018-09-07 13:14:58 +0700

    update code control

--- ClientApp/@app/controls/code.ts
+++ ClientApp/@app/controls/code.ts
@@ -1,7 +1,29 @@
 import { ko } from '@app/providers';
-
 import { handler } from '@app/common/ko';
-import { formatJs, formatXml } from '@app/common/pretty';
+
+const beautify = {
+    js: require('js-beautify').js,
+    css: require('js-beautify').css,
+    html: require('js-beautify').html,
+    configs: {
+        "indent_size": "4",
+        "indent_char": " ",
+        "max_preserve_newlines": "5",
+        "preserve_newlines": true,
+        "keep_array_indentation": false,
+        "break_chained_methods": false,
+        "indent_scripts": "normal",
+        "brace_style": "collapse",
+        "space_before_conditional": true,
+        "unescape_strings": false,
+        "jslint_happy": false,
+        "end_with_newline": false,
+        "wrap_line_length": "0",
+        "indent_inner_html": true,
+        "comma_first": false,
+        "e4x": false
+    }
+};
 
 import {
     Options,
@@ -12,13 +34,16 @@ import {
     htmlRender,
     init,
     process,
-
+    Result,
     // import preferred languages
     TypeScript,
-    XML
+    XML,
+    CSS,
+    SCSS,
+    JSON
 } from 'highlight-ts';
 
-registerLanguages(XML, TypeScript);
+registerLanguages(XML, TypeScript, SCSS, CSS, JSON);
 
 @handler({
     virtual: false,
@@ -29,22 +54,50 @@ export class I18nBindingHandler implements KnockoutBindingHandler {
         ko.bindingHandlers.html.init!(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext);
     }
     update = (element: HTMLElement, valueAccessor: any, allBindingsAccessor: any, viewModel: any, bindingContext: KnockoutBindingContext) => {
-        let code: string | object = ko.unwrap(valueAccessor()),
+        let lang: Result<string> = {
+            language: '',
+            relevance: 0,
+            value: ''
+        },
+            code: string | object = ko.unwrap(valueAccessor()),
+            type: string = ko.toJS(ko.unwrap(allBindingsAccessor().type) || ''),
             options: Options = { useBr: true, classPrefix: '' },
             highlighter: Highlighter<string> = init(htmlRender, options);
 
         if (typeof code === 'string') {
             if (String(code).indexOf('#') == 0) {
-                code = document.getElementById(String(code).replace(/#/, ''))!.outerHTML;
+                type = "xml";
+                code = beautify.html(document.getElementById(String(code).replace(/#/, ''))!.outerHTML, beautify.configs);
             } else {
-                code = formatJs(code);
+                lang = process(highlighter, String(code));
+                switch (type || lang.language) {
+                    case 'css':
+                        type = "css";
+                        code = beautify.css(code, beautify.configs);
+                        break;
+                    case 'scss':
+                        type = "scss";
+                        code = beautify.css(code, beautify.configs);
+                        break;
+                    case 'xml':
+                        type = "xml";
+                        code = beautify.html(code, beautify.configs);
+                        break;
+                    case 'typescript':
+                        type = "typescript";
+                        code = beautify.js(code, beautify.configs);
+                        break;
+                }
             }
         } else {
-            code = formatJs(code);
+            type = "json";
+            code = beautify.js(ko.toJSON(code), beautify.configs);
         }
 
-        let formater = process(highlighter, String(code));
+        lang = process(highlighter, String(code), type);
 
-        ko.bindingHandlers.html.update!(element, () => `<code class='${formater.language}'>${formater.value}</code>`, allBindingsAccessor, viewModel, bindingContext);
+        console.log(code);
+
+        ko.bindingHandlers.html.update!(element, () => `<code class='${type}'>${lang.value}</code>`, allBindingsAccessor, viewModel, bindingContext);
     }
 }
