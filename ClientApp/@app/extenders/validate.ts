519c808f822ee875db5057696cb16f87c8b7bd1d
Nguyễn Tuệ Vương <vungtroixa@gmail.com>
2018-09-10 23:42:17 +0700

    update validate param

--- ClientApp/@app/extenders/validate.ts
+++ ClientApp/@app/extenders/validate.ts
@@ -2,7 +2,7 @@ import { _, ko } from '@app/providers';
 import { extend } from '@app/extenders/validation';
 
 ko.utils.extend(ko.extenders, {
-    validate: (target: ValidationObservable<any>, params: () => string | undefined) => {
+    validate: (target: ValidationObservable<any>, params: (value: any) => string | undefined) => {
         //add some sub-observables to our observable
         extend(target);
 
