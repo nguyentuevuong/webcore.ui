bdb4b8925bb303c7aa461938ca77dd84ffb551b0
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2018-09-11 18:59:11 +0700

    remove code length extenders

--- ClientApp/@app/extenders/length.ts
+++ ClientApp/@app/extenders/length.ts
@@ -2,17 +2,8 @@ import { _, ko } from '@app/providers';
 import { extend } from '@app/extenders/validation';
 
 ko.utils.extend(ko.extenders, {
-    length: (target: ValidationObservable<number>, length: { min: number, max: number }) => {
+    length: (target: ValidationObservable<string>, length: { min: number, max: number }) => {
         extend(target);
-
-        // extend name prop of observable
-        if (_.has(target, '$name')) {
-            target.$name!(ko.toJS(name));
-        } else {
-            ko.utils.extend(target, {
-                $name: ko.observable(ko.toJS(name))
-            });
-        }
 
         return target;
     }
