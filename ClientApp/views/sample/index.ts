f2014c86332c4ff4fe63c9b2188ee089788306bc
Nguyễn Tuệ Vương <vungtroixa@gmail.com>
2018-09-26 19:03:45 +0700

    remove some view

--- ClientApp/views/sample/index.ts
+++ ClientApp/views/sample/index.ts
@@ -1,10 +1,6 @@
 
 import './home';
 import './icons';
-import './json';
-import './jquery';
-import './class';
-import './focus';
 import './text-editor/editor';
 import './date-picker/date-picker';
 
@@ -12,12 +8,6 @@ import './modal/modal.parent';
 import './modal/modal.childrent';
 import './modal/modal.childrent.2';
 
-import './like-widget';
-import './deferred/deferred.1';
-import './deferred/deferred.2';
-import './deferred/deferred.3';
-import './deferred/deferred';
-import './worker/worker';
 import './employee/employee';
 
 import './selections/dropdown/dropdown';
