8e0e2f8170c047c58a63518388da6b1867d46861
Nguyễn Tuệ Vương <vungtroixa@gmail.com>
2018-10-08 23:59:54 +0700

    group ui & ko binding

--- ClientApp/@app/controls/index.ts
+++ ClientApp/@app/controls/index.ts
@@ -1,30 +1,19 @@
-import '@app/controls/i18n';
-
-import "@app/controls/init";
-import '@app/controls/switch';
-
 import "@app/controls/alert";
 
 import "./class-ko-ext";
 import "./check-ko-ext";
 
-import '@app/controls/each';
-import "@app/controls/label";
+
+
+
+import "@app/controls/ko";
+import "@app/controls/ui";
 import '@app/controls/input';
 import '@app/controls/selection';
-import '@app/controls/safe-click';
 
-import "@app/controls/sortable";
-
-import '@app/controls/modal';
-import '@app/controls/tabs';
-import '@app/controls/wizard';
-import '@app/controls/slide-panel';
 
 import "./combobox-ko-ext";
 import "./datepicker-ko-ext";
 import "./texteditor-ko-ext";
-
-import './click-out-ko-ext';
 
 import '@app/controls/code';
---
+++ ClientApp/@app/controls/ui/index.ts
@@ -0,0 +1,5 @@
+import '@app/controls/ui/tabs';
+import '@app/controls/ui/label';
+import '@app/controls/ui/modal';
+import '@app/controls/ui/wizard';
+import '@app/controls/ui/slide-panel';
