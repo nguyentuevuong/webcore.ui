7d1711436fa9d895487885b9ab6867b1b149e32c
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2018-10-08 11:35:30 +0700

    remove lodash from random id func

--- ClientApp/@app/common/id/random.ts
+++ ClientApp/@app/common/id/random.ts
@@ -1,3 +1,1 @@
-import * as _ from 'lodash';
-
-export const randomId = (): string => _(Math.random().toString(36)).slice(2, 33).join('');
+export const randomId = (): string => Math.random().toString(36).slice(2, 12);
