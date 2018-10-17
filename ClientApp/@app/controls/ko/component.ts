3dc0ad9a130595a39cbcd90ac638ebebb032539a
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2018-10-17 19:30:53 +0700

    add new custom component binding

---
+++ ClientApp/@app/controls/ko/component.ts
@@ -0,0 +1,21 @@
+import * as _ from 'lodash';
+import * as ko from 'knockout';
+
+import { handler } from '@app/common/ko';
+
+let origC = ko.bindingHandlers.component;
+
+@handler({
+    virtual: true,
+    bindingName: 'component'
+})
+export class CustomComponentBindingHandler implements KnockoutBindingHandler {
+    init = (element: HTMLElement, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) => {
+
+        origC.init!(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext);
+
+        debugger;
+
+        return { controlsDescendantBindings: true };
+    }
+}
--- ClientApp/@app/controls/ko/index.ts
+++ ClientApp/@app/controls/ko/index.ts
@@ -2,6 +2,7 @@ import '@app/controls/ko/i18n';
 import '@app/controls/ko/init';
 import '@app/controls/ko/each';
 import '@app/controls/ko/switch';
+import '@app/controls/ko/component';
 import '@app/controls/ko/sortable';
 import '@app/controls/ko/safe-click';
 import '@app/controls/ko/click-out-ko-ext';
