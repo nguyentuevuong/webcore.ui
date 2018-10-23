a7a3007062368846a024f1d855365244eb12bdf0
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2018-10-23 12:51:05 +0700

    update fxtable binding

--- ClientApp/@app/controls/ui/fixed-table.ts
+++ ClientApp/@app/controls/ui/fixed-table.ts
@@ -1,19 +1,45 @@
-
 import { ko } from '@app/providers';
 import { handler } from '@app/common/ko';
 import { fxTable } from '@app/common/utils/fxtable';
+
+let ki = '__fxtable__',
+    nasc = '__fxaccessor__',
+    extend = ko.utils.extend,
+    domData = ko.utils.domData,
+    unwrapObs = ko.utils.unwrapObservable;
 
 @handler({
     virtual: false,
     bindingName: 'fxtable'
 })
 export class FixedTableBindingHandler implements KnockoutBindingHandler {
-    init = (element: HTMLTableElement, valueAccessor: () => { width: number; displayRow: number; fixedColumn: number; }, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) => {
-        let newAccessor = () => ({ afterRender: () => { new fxTable(element, ko.toJS(valueAccessor())); } });
+    init = (element: HTMLTableElement, valueAccessor: () => { width: number; displayRow: number; fixedColumn: number; columns: Array<number>; }, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) => {
+        let asr = valueAccessor(),
+            newAccessor = () => ({
+                afterRender: () => {
+                    domData.set(element, ki, new fxTable(element, ko.toJS(asr)));
+                }
+            });
 
         ko.bindingHandlers.template.init!(element, newAccessor, allBindingsAccessor, viewModel, bindingContext);
         ko.bindingHandlers.template.update!(element, newAccessor, allBindingsAccessor, viewModel, bindingContext);
 
         return { controlsDescendantBindings: true };
+    }
+    update = (element: HTMLTableElement, valueAccessor: () => { width: number; displayRow: number; fixedColumn: number; columns: Array<number>; }, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) => {
+        let accessor = valueAccessor(),
+            fxt: fxTable = domData.get(element, ki),
+            options = {
+                width: unwrapObs(accessor.width),
+                columns: unwrapObs(accessor.columns),
+                displayRow: unwrapObs(accessor.displayRow),
+                fixedColumn: unwrapObs(accessor.fixedColumn)
+            };
+
+        if (fxt) {
+            extend(fxt.options, options);
+
+            fxt.initLayout();
+        }
     }
 }
