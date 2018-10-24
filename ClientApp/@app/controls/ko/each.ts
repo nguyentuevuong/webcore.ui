ad49ad819cbc32ceebd323a13089103c8097bedb
Nguyễn Tuệ Vương <vungtroixa@gmail.com>
2018-10-24 22:43:01 +0700

    update each binding

--- ClientApp/@app/controls/ko/each.ts
+++ ClientApp/@app/controls/ko/each.ts
@@ -1,21 +1,21 @@
-import * as _ from 'lodash';
-import * as ko from 'knockout';
-
+import { ko } from '@app/providers';
 import { handler } from '@app/common/ko';
 
 @handler({
     virtual: true,
-    bindingName: 'each'
+    bindingName: 'each',
+    validatable: true
 })
 export class EachBindingHandler implements KnockoutBindingHandler {
-    init = (element: HTMLElement, valueAccessor: any, allBindingsAccessor: any, viewModel: any, bindingContext: KnockoutBindingContext) => {
+    init = (element: HTMLElement, valueAccessor: any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) => {
         return ko.bindingHandlers.foreach.init!(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext);
     }
     update = (element: HTMLElement, valueAccessor: any, allBindingsAccessor: any, viewModel: any, bindingContext: KnockoutBindingContext) => {
         let array = ko.utils.unwrapObservable(valueAccessor().data || valueAccessor());
 
         ko.bindingHandlers.foreach.update!(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext.extend({
-            "$length": array.length
+            "$length": array.length,
+            "$lastIndex": (array.length || 1) - 1
         }));
     }
 }
