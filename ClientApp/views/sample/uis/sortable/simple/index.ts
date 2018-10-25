d8b30fdce08d65a058b7bcaeb1495cbc43842e62
Nguyễn Tuệ Vương <vungtroixa@gmail.com>
2018-10-26 00:05:35 +0700

    update sample sortable

--- ClientApp/views/sample/uis/sortable/simple/index.ts
+++ ClientApp/views/sample/uis/sortable/simple/index.ts
@@ -1,93 +1,34 @@
 import { _, ko } from '@app/providers';
 import { component } from '@app/common/ko';
 
+import { Sortable } from '@app/common/ui/sortable';
+
 @component({
     url: 'sample/sortable/simple',
     name: 'sample-sortable-simple',
     title: 'Sortable control (simple)',
     icon: 'fa fa-refresh',
-    styles: require('./style.css'),
+    styles: require('./style.scss'),
     template: require('./index.html'),
     resources: {
         'en': {
             'sample|sortable': 'Sortables'
         },
         'vi': {
-            'sample|sortable': 'Điều khiển sắp xếp'            
+            'sample|sortable': 'Điều khiển sắp xếp'
         }
     }
 })
 export class SampleSortableSimpleViewModel {
-    highPriorityTasks: KnockoutObservableArray<Task> = ko.observableArray([
-        new Task("Get dog food"),
-        new Task("Mow lawn"),
-        new Task("Fix car")
-    ]);
-    normalPriorityTasks: KnockoutObservableArray<Task> = ko.observableArray([
-        new Task("Fix fence"),
-        new Task("Walk dog"),
-        new Task("Read book")
-    ]);
-
-    trash: KnockoutObservableArray<Task> = ko.observableArray([]);
-    selectedTask: KnockoutObservable<Task | undefined> = ko.observable();
-
     constructor(params: any, private element: HTMLElement) {
         let self = this;
 
-        ko.utils.extend(self.highPriorityTasks, {
-            id: 'high'
-        });
-
-        ko.utils.extend(self.normalPriorityTasks, {
-            id: 'normal'
-        });
-
-        ko.utils.extend(self.trash, {
-            id: 'trash'
-        });
     }
 
-    clearTask(data: Task, event: any) {
+    afterRender() {
         let self = this;
 
-        if (_.isEqual(ko.toJS(data), ko.toJS(self.selectedTask))) {
-            self.selectedTask(undefined);
-        }
-
-        if (data.name() === "") {
-            self.highPriorityTasks.remove(data);
-            self.normalPriorityTasks.remove(data);
-        }
-    };
-
-    isTaskSelected(task: Task) {
-        let self = this;
-
-        return task === self.selectedTask();
-    };
-
-    addTask() {
-        let self = this,
-            task = new Task("new");
-
-        self.selectedTask(task);
-        self.normalPriorityTasks.push(task);
-    };
-
-    myDropCallback(arg: any) {
-        if (console) {
-            console.log("Moved '" + arg.item.name() + "' from " + arg.sourceParent.id + " (index: " + arg.sourceIndex + ") to " + arg.targetParent.id + " (index " + arg.targetIndex + ")");
-        }
-    };
-}
-
-class Task {
-    name: KnockoutObservable<string> = ko.observable('');
-
-    constructor(name: string) {
-        let self = this;
-
-        self.name(name || '');
+        new Sortable(self.element.querySelector('#list_1'));
+        new Sortable(self.element.querySelector('#list_2'));
     }
 }
