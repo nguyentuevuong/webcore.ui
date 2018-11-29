dd0b517c9b9d6c93692a1efe66c7c59a225c0eeb
Việt Nam <vietnam@Viets-Mac-mini.local>
2018-11-29 10:33:32 +0700

    update label control (remove component)

--- ClientApp/@app/controls/ui/label.ts
+++ ClientApp/@app/controls/ui/label.ts
@@ -1,30 +1,46 @@
 import { ko } from '@app/providers';
-
 import { handler } from '@app/common/ko';
+import { i18text } from '@app/common/lang';
 
 @handler({
     bindingName: 'label'
 })
 export class LabelControlBindingHandler implements KnockoutBindingHandler {
-    init = (element: HTMLElement, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) => {
-        let control: ValidationObservable<any> = valueAccessor();
-
-        // bind label component
-        ko.bindingHandlers.component.init!(element, () => ({
-            name: 'label', params: { control: control }
-        }), allBindingsAccessor, viewModel, bindingContext);
-
-        // bind css
-        ko.bindingHandlers.css.update!(element, () => ({
-            'control-label control-label-block mb-1': true,
-            'control-label-danger': ko.toJS(control.$require)
-        }), allBindingsAccessor, viewModel, bindingContext);
+    init = (element: HTMLElement, valueAccessor: () => ValidationObservable<any>, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) => {
+        let dom = ko.utils.dom,
+            control = valueAccessor();
 
         // bind for attr
-        ko.bindingHandlers.attr.update!(element, () => ({
-            'for': ko.toJS(control.$attr).id
-        }), allBindingsAccessor, viewModel, bindingContext);
+        dom.setAttr(element, 'for', ko.toJS(control.$attr).id);
+
+        dom.addClass(element, 'control-label control-label-block mb-1');
 
         return { controlsDescendantBindings: true };
+    }
+    update = (element: HTMLElement, valueAccessor: () => ValidationObservable<any>, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) => {
+        let dom = ko.utils.dom,
+            control = valueAccessor(),
+            $name = ko.toJS(control.$name),
+            $require = ko.toJS(control.$require),
+            $constraint = ko.toJS(control.$constraint);
+
+        element.innerHTML = '';
+
+        if ($require) {
+            dom.addClass(element, 'control-label-danger');
+        } else {
+            dom.removeClass(element, 'control-label-danger');
+        }
+
+        if (!$name) {
+            dom.addClass(element, 'd-none');
+        } else {
+            dom.removeClass(element, 'd-none');
+            element.appendChild(dom.create('span', { text: i18text($name) }));
+        }
+
+        if ($constraint) {
+            element.appendChild(dom.create('span', { text: i18text($constraint) }));
+        }
     }
 }
